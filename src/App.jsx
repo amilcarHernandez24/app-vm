73f1b5a6e177dad056a0e43c32ba3b2c66246de8
Augele Alejos <augelealejos@gmail.com>
2023-07-17 15:31:13 -0400

    [Add] footer structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import vmLogo from './assets/img/vm-logo.svg'
 import './App.css'
+import Footer from './components/footer'
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
       <div className='container'>
         <img src={vmLogo} className='main_logo' alt="VM logo" />
       </div>
+      <Footer>
+      
+      </Footer>
     </>
   )
 }
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import styles from './footer.module.css'
+
+function Footer() {
+
+  return (
+    <>
+      <footer className={styles.footer}>
+        <h1>Footer</h1>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
