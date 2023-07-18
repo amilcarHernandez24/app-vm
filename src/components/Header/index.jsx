c3666678fbfbd31f8437192b5e8b3b4fbcd93b2f
Amilcar <amilcarjh24@gmail.com>
2023-07-18 12:19:21 -0300

    [Add] header feature and styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
-import './header.module.css'
+import styles from './header.module.css'
+import logo from '../../assets/img/logo-header.svg'
 
 function Header () {
   return (
     <>
-      <header>
-        
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt='logo'/>
+        <div className={styles.header_content}>
+          <h2 className={styles.header_title}>Portfolio Radar N2</h2>
+          <p className={styles.header_user}>Nombre del colaborador</p>
+        </div>
       </header>
     </>
   )
